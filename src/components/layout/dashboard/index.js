78e62b9537dbd945461cf0da53676513912e56e3
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-04-12 15:51:19 +0200

    Added Surveys to the redux store

--- src/components/layout/dashboard/index.js
+++ src/components/layout/dashboard/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SideBar from "../../ui/sideBar";
 import Header from "@/components/ui/header";
 import SettingPanel from "@/components/ui/panels/SettingPanel";
@@ -15,10 +15,33 @@ import {
 import { signOut } from "next-auth/react";
 
 import ViewSurveyPanel from "@/components/ui/panels/ViewSurveyPanel";
+import { getAllAnswers, getAllSurveys } from "@/utils/surveyApi";
+import { useDispatch } from "react-redux";
+import { setSurveys } from "@/components/base/store/surveySlice";
+import { setAnswers } from "@/components/base/store/answerSlice";
 
 const Dashboard = ({ user, surveys, users }) => {
+  const dispatch = useDispatch();
+
   const [sidebarState, setSideBarState] = useState(true);
   const [showActivePanel, setShowActivePanel] = useState("Overview");
+
+  const getAllSurveys_Call = async () => {
+    const surveys_ = await getAllSurveys()
+
+    dispatch(setSurveys(surveys_))
+    const answers_ = await getAllAnswers()
+
+    dispatch(setAnswers(answers_))
+
+  }
+  useEffect(() => {
+    getAllSurveys_Call().then(() => {
+      //you can set a loader here
+    })
+  }, [])
+
+
 
   const toggleSidebarState = () => setSideBarState(!sidebarState);
   const closeSideBar = () => {
@@ -39,21 +62,21 @@ const Dashboard = ({ user, surveys, users }) => {
       <DashboardMain>
         <Header user={user} toggleSidebarState={toggleSidebarState} setShowActivePanel={setShowActivePanel} />
         <MainContent onClick={closeSideBar} sideBarOpen={sidebarState}>
-        {showActivePanel === "Overview" ? (
-          <SettingPanel users={users} />
-        ) : showActivePanel === "My Account" ? (
-          <AccountPanel />
-        ) : showActivePanel === "Library" ? (
-          <LibraryPanel />
-        ) : showActivePanel === "Contacts" ? (
-          <ContactPanel />
-        ) : showActivePanel === "Update to SSO" ? (
-          <UpdateSSOPanel />
-        ) : showActivePanel === "Logout" ? (
-          <Logout onClick={signOut()} />
-        ) : (
-          <ViewSurveyPanel />
-        )}
+          {showActivePanel === "Overview" ? (
+            <SettingPanel users={users} />
+          ) : showActivePanel === "My Account" ? (
+            <AccountPanel />
+          ) : showActivePanel === "Library" ? (
+            <LibraryPanel />
+          ) : showActivePanel === "Contacts" ? (
+            <ContactPanel />
+          ) : showActivePanel === "Update to SSO" ? (
+            <UpdateSSOPanel />
+          ) : showActivePanel === "Logout" ? (
+            <Logout onClick={signOut()} />
+          ) : (
+            <ViewSurveyPanel />
+          )}
           {/* {showActivePanel === "Overview" ? (
             <SettingPanel users={users} />
           ) : showActivePanel === "My Account" ? (
